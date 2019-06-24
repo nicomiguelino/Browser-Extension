796a187c5a5b159ed66040a655800918b70b53d4
Alexander Ljungberg <aljungberg@slevenbits.com>
2019-06-24 17:30:04 +0100

    New: state class to save what we've done in the past in synced storage.

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,3 +114,70 @@ function getWebAsset(assetId, user) {
 function getAssetDashboardLink(assetId) {
     return `https://login.screenlyapp.com/login?next=/manage/assets/${assetId}`;
 }
+
+
+class State {
+    constructor() {
+    }
+
+    // Make a new URL equivalent to the given URL but in a normalized format.
+    static normalizeUrl(url) {
+        return window.normalizeUrl(url, {
+            removeTrailingSlash: false,
+            sortQueryParameters: false,
+            stripWWW: false,
+        });
+    }
+
+    // Simplify a URL heavily, even if it slightly changes its meaning.
+    static simplifyUrl(url) {
+        return window.normalizeUrl(url, {
+            removeTrailingSlash: true,
+            sortQueryParameters: true,
+            stripHash: true,
+            stripProtocol: true,
+            stripWWW: true,
+        })
+    }
+
+    static setAssetId(url, assetId) {
+        url = State.simplifyUrl(url);
+        console.debug(`Saving ${url} -> ${assetId}`);
+
+        return browser.storage.sync.get(['state'])
+            .then((state) => {
+                state = state || {};
+
+                if (assetId)
+                    state[url] = assetId;
+                else
+                    delete state[url];
+
+                console.debug("State: ", state);
+                return browser.storage.sync.set({'state': state})
+                    .catch((error) => {
+                        console.error("Unable to save state %s -> %s", url, assetId);
+                        // Assume it's because storage is full. Try to set just one key.
+                        // TODO Use LRU to ensure the dictionary doesn't ever grow larger than the
+                        // sync storage limit.
+                        return browser.stoage.sync.remove('state').then(() => {
+                            if (assetId)
+                                return browser.storage.sync.set({'state': {url: assetId}});
+                            else
+                                return browser.storage.sync.set({'state': {}});
+                        });
+                    });
+            });
+    }
+
+    static getAssetId(url) {
+        url = State.simplifyUrl(url);
+        console.debug(`Reading back ${url}`);
+
+        return browser.storage.sync.get(['state'])
+            .then(({state}) => {
+                state = state || {};
+                return state[url];
+            });
+    }
+}
