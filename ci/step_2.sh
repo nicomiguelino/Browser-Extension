f52fb1b7114984e3ef7e933bfb29dd8157b9aa95
Alexander Ljungberg <aljungberg@slevenbits.com>
2019-07-12 16:26:03 +0100

    Fixed: npm teamcity isn't a thing.

--- ci/step_2.sh
+++ ci/step_2.sh
@@ -5,4 +5,4 @@
 docker run \
     --rm \
     sce_webpack:latest \
-    /bin/bash -c "npx webpack --config webpack.dev.js && npm teamcity"
+    /bin/bash -c "npx webpack --config webpack.dev.js && npm run teamcity"
